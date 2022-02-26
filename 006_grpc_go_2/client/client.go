c6ad173cd701e79ff015a1ab6077f8a0a0934799
ck996 <ck996>
2022-02-26 17:42:41 +0800

    第一个案例跟完

--- 006_grpc_go_2/client/client.go
+++ 006_grpc_go_2/client/client.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"context"
+	"fmt"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"n01/006_grpc_go_2/proto"
+)
+
 func main() {
+	//conn, _ := grpc.Dial("localhost:8080", grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	c := proto.NewGreeterClient(conn)
+	r, err := c.SayHello(context.Background(), &proto.HelloRequest{Name: "bobby"})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(r)
 
 }
--- 006_grpc_go_2/server/server.go
+++ 006_grpc_go_2/server/server.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"context"
+	"net"
 
+	"google.golang.org/grpc"
+
+	"n01/006_grpc_go_2/proto"
+)
+
+type Server struct {
+}
+
+func (s *Server) SayHello(ctx context.Context, request *proto.HelloRequest) (*proto.HelloReply, error) {
+	return &proto.HelloReply{
+		Message: "hello " + request.Name,
+	}, nil
+}
+
+func main() {
+	//实例化 grpc
+	g := grpc.NewServer()
+	proto.RegisterGreeterServer(g, &Server{})
+	//启动服务
+	listener, err := net.Listen("tcp", "0.0.0.0:8080")
+	if err != nil {
+		panic(err)
+	}
+	err = g.Serve(listener)
+	if err != nil {
+		panic(err)
+	}
 }
